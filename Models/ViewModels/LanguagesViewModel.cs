9f53ba8b5dec59758a4854ba2f2223c888accad0
Yinzy00 <yari.marien@telenet.be>
2023-03-09 23:05:38 +0100

    Added viewmodels for translation controller

---
+++ Models/ViewModels/LanguagesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Our.Umbraco.Synthscribe.Models.ViewModels
+{
+    internal class LanguagesViewModel
+    {
+        [JsonProperty("defaultLanguage")]
+        public string DefaultLanguage { get; set; }
+        [JsonProperty("languages")]
+        public List<string> Languages { get; set; } = new List<string>();
+    }
+}
---
+++ Models/ViewModels/TranslateDictionaryViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Our.Umbraco.Synthscribe.Models.ViewModels
+{
+    public class TranslateDictionaryViewModel
+    {
+        [JsonProperty("dictionaryId")]
+        public int DictionaryId { get; set; }
+        [JsonProperty("languageTo")]
+        public string LanguageTo { get; set; }
+        [JsonProperty("overwrite")]
+        public bool Overwrite { get; set; }
+        [JsonProperty("translateDescendants")]
+        public bool TranslateDescendants { get; set; }
+    }
+}

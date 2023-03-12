869d4606c2cdf1ac0ea754d95a6e6ac02cbd05d3
Yinzy00 <yari.marien@telenet.be>
2023-03-12 02:03:11 +0100

    Added parent for translation services

---
+++ Services/TranslateUmbracoService.cs
@@ -0,0 +1,22 @@
+﻿using Our.Umbraco.Synthscribe.Services.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Services;
+
+namespace Our.Umbraco.Synthscribe.Services
+{
+    internal class TranslateUmbracoService
+    {
+        public readonly IEnumerable<ILanguage> languages;
+        public readonly ILanguage defaultLanguage;
+        public TranslateUmbracoService(ILocalizationService localizationService)
+        {
+            languages = localizationService.GetAllLanguages().Where(l => !l.IsDefault);
+            defaultLanguage = localizationService.GetAllLanguages().First(l => l.IsDefault);
+        }
+    }
+}

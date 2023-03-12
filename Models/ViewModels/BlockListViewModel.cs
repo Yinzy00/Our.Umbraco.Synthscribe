146c1d256fbf0536acc2238982f3cd9ac8665fef
Yinzy00 <yari.marien@telenet.be>
2023-03-12 02:04:20 +0100

    Added content translation viewmodels

---
+++ Models/ViewModels/BlockListViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace OpenAi.Models.ViewModels
+{
+    public class BlockListLayoutViewModel
+    {
+        [JsonProperty("Umbraco.BlockList")]
+        public List<UmbracoBlockList> UmbracoBlockList { get; set; } = new List<UmbracoBlockList>();
+    }
+
+    public class BlockListViewModel
+    {
+        [JsonProperty("layout")]
+        public BlockListLayoutViewModel Layout { get; set; }
+        [JsonProperty("contentData")]
+        public List<JObject> ContentData { get; set; }
+        [JsonProperty("settingsData")]
+        public List<object> SettingsData { get; set; }
+
+    }
+
+    public class UmbracoBlockList
+    {
+        [JsonProperty("contentUdi")]
+        public string ContentUdi { get; set; }
+    }
+
+
+}
---
+++ Models/ViewModels/MultiUrlPickerViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenAi.Models.ViewModels
+{
+    internal class MultiUrlPickerViewModel
+    {
+        [JsonProperty("name")]
+        public string? Name { get; set; }
+        [JsonProperty("url")]
+        public string? Url { get; set; }
+        [JsonProperty("queryString")]
+        public string? QueryString { get; set; }
+    }
+}

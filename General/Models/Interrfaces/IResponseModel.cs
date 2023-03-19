407481cf3a69966b48e663cbec7e43d742e8e898
Yinzy00 <yari.marien@telenet.be>
2023-03-19 01:10:09 +0100

    Added response model

---
+++ General/Models/Interrfaces/IResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Our.Umbraco.Synthscribe.General.Models.Interrfaces
+{
+    public interface IResponseModel
+    {
+        public bool Succes { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ General/Models/ResponseModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Our.Umbraco.Synthscribe.General.Models.Interrfaces
+{
+    public class ResponseModel: IResponseModel
+    {
+        public ResponseModel()
+        {
+
+        }
+        public ResponseModel(bool succes, string message)
+        {
+            Succes = succes;
+            _message = message;
+        }
+        private string _message;
+        public bool Succes { get; set; }
+        public string Message { get => _message ?? "Seomthing went wrong."; set => _message = value; }
+    }
+}
